3f78218ed62c38f355686a62edee2d434e997351
Michael Mao <michael.mao@fiserv.com>
2024-04-25 15:36:39 -0400

    First commit

---
+++ src/main/java/com/michael/mao/redistutorials/RedisTutorialsApplication.java
@@ -0,0 +1,12 @@
+package com.michael.mao.redistutorials;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisTutorialsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RedisTutorialsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/michael/mao/redistutorials/RedisTutorialsApplicationTests.java
@@ -0,0 +1,12 @@
+package com.michael.mao.redistutorials;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisTutorialsApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+
+}
